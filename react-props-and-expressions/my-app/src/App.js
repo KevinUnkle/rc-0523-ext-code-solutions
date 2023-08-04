d5ae00ed6afda0732984c499809afc2d1b6a4250
Kevin Unkle <kevinunk@gmail.com>
2023-08-04 22:03:54 +0000

    Completed react-props-and-expressions exercise.

---
+++ react-props-and-expressions/my-app/src/App.js
@@ -0,0 +1,11 @@
+import CustomButton from './CustomButton';
+
+export default function App() {
+  return (
+    <div>
+      <CustomButton text="I" color="red" />
+      <CustomButton text="know" color="green" />
+      <CustomButton text="React!" color="lightblue" />
+    </div>
+  );
+}
