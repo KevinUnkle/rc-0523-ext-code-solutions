06ce1c4102fa9137e1f292b2a3e4139fa81f0277
Kevin Unkle <kevinunk@gmail.com>
2023-08-07 15:50:29 +0000

    deleted unused code

--- react-events/my-app/src/CustomButton.js
+++ react-events/my-app/src/CustomButton.js
@@ -1,7 +1,4 @@
 export default function CustomButton({ text, color, onCustomClick }) {
-  function handleClick() {
-    alert(`Button ${text} clicked!`);
-  }
   return (
     <button
       onClick={() => onCustomClick(text)}
