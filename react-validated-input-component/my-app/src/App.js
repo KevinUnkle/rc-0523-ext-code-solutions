2f4b1eb6126ab9f7524637051e738cc9e403efa0
Kevin Unkle <kevinunk@gmail.com>
2023-08-08 21:58:55 +0000

    Completed react-validated-input-component exercise.

---
+++ react-validated-input-component/my-app/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import ValidatedInput from './ValidatedInput';
+
+export default function App() {
+  return (
+    <div className="App">
+      Password
+      <ValidatedInput />
+    </div>
+  );
+}
