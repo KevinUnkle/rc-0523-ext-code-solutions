520d2ebd51edc6c44a7ee6b261e4b575888bfe9e
Kevin Unkle <kevinunk@gmail.com>
2023-08-09 18:22:03 +0000

    Completed phase 1

---
+++ react-design/my-app/src/RotatingBanner.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default function RotatingBanner({ items }) {
+  const current = 1;
+  return (
+    <>
+      <Banner title={items[1]} />
+      <PrevButton />
+      <br />
+      <Indicators count={items.length} currentIndex={current} />
+      <br />
+      <NextButton />
+    </>
+  );
+}
+
+function Banner({ item }) {
+  return <div>{item}</div>;
+}
+
+function NextButton({ onNext }) {
+  return (
+    <button type="button" onClick={onNext} style={{ backgroundColor: 'white' }}>
+      Next
+    </button>
+  );
+}
+
+function PrevButton({ onPrev }) {
+  return (
+    <button type="button" onClick={onPrev} style={{ backgroundColor: 'white' }}>
+      Prev
+    </button>
+  );
+}
+
+function Indicators({ count, current, onSelect }) {
+  const buttons = [];
+  for (let i = 0; i < count; i++) {
+    buttons.push(
+      <button
+        key={i}
+        type="button"
+        onClick={() => onSelect(i)}
+        style={{ backgroundColor: i === current ? 'lightblue' : 'white' }}>
+        {i}
+      </button>
+    );
+  }
+  return <div>{buttons}</div>;
+}
